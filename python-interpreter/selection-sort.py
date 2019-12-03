1351d532e9c876da710ecb67fcc231c60baf22ff
manuhana <58454305+manuhana@users.noreply.github.com>
2019-12-03 16:34:21 +0700

    Update selection-sort.py

--- python-interpreter/selection-sort.py
+++ python-interpreter/selection-sort.py
@@ -2,17 +2,17 @@
 xlist = [2,7,9,13,8,21,5]
 
 # defining selection-sort function
-def selectionsort( xlist ):
-  for i in range( len( xlist ) ):
+def selectionsort(xlist):
+  for i in range(len(xlist)):
     minimum = i
-    for k in range( i + 1 , len( xlist ) ):
+    for k in range(i+1, len(xlist)):
       if xlist[k] < xlist[minimum]:
         minimum = k
  
-    swap( xlist, minimum, i )
+    swap(xlist, minimum, i)
  
 # making swap function 
-def swap( A, x, y ):
+def swap(A, x, y):
   tmp = A[x]
   A[x] = A[y]
   A[y] = tmp
