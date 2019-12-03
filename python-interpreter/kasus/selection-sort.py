56a3b51d08f8f9c0070ae3b480d7197eba7d3cb3
manuhana <58454305+manuhana@users.noreply.github.com>
2019-12-03 21:07:30 +0700

    update selection-sort
    remove list and print result

--- python-interpreter/kasus/selection-sort.py
+++ python-interpreter/kasus/selection-sort.py
@@ -1,7 +1,3 @@
-# defining list
-xlist = [2,7,9,13,8,21,5]
-
-# defining selection-sort function
 def selectionsort( xlist ):
   for i in range( len( xlist ) ):
     minimum = i
@@ -10,15 +6,10 @@ def selectionsort( xlist ):
         minimum = k
  
     swap( xlist, minimum, i )
- 
-# making swap function 
+  
 def swap( A, x, y ):
   tmp = A[x]
   A[x] = A[y]
   A[y] = tmp
 
-# run selection-sort function
 selectionsort(xlist)
-
-# print out the result
-print(xlist)
