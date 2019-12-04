53c62e91d5e420c697a3c08c017f5a27d65228a7
manuhana <rinut.manuhana@gmail.com>
2019-12-05 02:19:54 +0700

    Added tupples, sets, and dictionaries

--- python-interpreter/excercise/data-structures.py
+++ python-interpreter/excercise/data-structures.py
@@ -41,3 +41,39 @@ queue.append('fish')
 queue.append('ant')
 queue.popleft()
 queue
+
+# lists comprehension
+power_three = []
+for x in range(10):
+    power_three.append(x**3)
+power_three
+
+# similar as
+power_three = [x**3 for x in range(10)]
+power_three
+
+# tupples
+tup_one = 'ceb', 'ana', 666
+tup_two = tup_one, ('notail', 'jerax', 999)
+tup_two
+
+# sets
+set_1 = set(wkwkwkwk)
+set_2 = set(okokokok)
+set_1
+set_2
+set_1 ^ set_2
+
+# dictionaries
+exp = {'ana': 12, 'ceb': 7, 'notail': -1, 'ban': 0}
+exp['ana']
+del exp['ban']
+exp
+
+# other ways
+exp_2([('ban', 18), ('dea', 21), ('zack', 33)])
+exp_2
+
+# or
+exp_3(miracle=9856, ana=9722, pevo=1282)
+exp_3
