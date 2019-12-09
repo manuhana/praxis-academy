aba86da7e81f56ba829d902f89a7c27b8dbfe0b6
manuhana <rinut.manuhana@gmail.com>
2019-12-09 17:14:09 +0700

    updated format

--- python-interpreter/case/bubble_sort.py
+++
@@ -1,7 +0,0 @@
-def bubble_sort(nlist):
-    for a in range(len(nlist)-1,0,-1):
-        for i in range(a):
-            if nlist[i]>nlist[i+1]:
-                temp = nlist[i]
-                nlist[i] = nlist[i+1]
-                nlist[i+1] = temp
