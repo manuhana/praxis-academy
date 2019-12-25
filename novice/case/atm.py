a993287d5bd8262b15b050b8740deca6b685c519
manuhana <rinut.manuhana@gmail.com>
2019-12-25 15:32:58 +0700

    new build

--- novice/case/atm.py
+++ novice/case/atm.py
@@ -1,7 +1,7 @@
 # classing machine opening
-print('###################################')
-print('Welcome to Manuhana ATM!')
-print('###################################')
+print('######################################')
+print('\tWelcome to Manuhana ATM!')
+print('######################################')
 
 # let's get started with welcome menu
 print('Do you have an account?')
@@ -9,19 +9,33 @@ print('[1] Yes')
 print('[2] No, I want to sign up now')
 decide = input()
 
-# main menu for login or sign up
-if decide == '1':
-	# existing account
-	x_username = input('Please input your username : ')
-	x_pin = input('Please input your pin number : ')
-	print('Welcome,', x_username.title() + '!')
+#  this will is the client username and pin database
+client = {}
 
-else:
-	# create new account
+# create a new account
+def sign_up():
 	username = input('Please input a username : ')
 	print('Please create your 6 number of pin')
 	pin = input()
+	# checking pin length
+	while len(pin) != 6:
+		print('Your pin number must be 6 number!')
+		print('Please create your 6 number of pin')
+		pin = input()
+	print('Account created!')
 	print('Welcome,', username.title() + '!')
+	client[username] = pin
+	return client
 
-# lets make a client database
-client = {}
+# login system
+def login():
+	username = input('Please input your username: ')
+	print('Please input your pin number')
+	pin = input()
+	# lets check wether the username and pin are already registered
+
+# lets back to decide action
+if decide == '1':
+	login()
+else:
+	sign_up()
